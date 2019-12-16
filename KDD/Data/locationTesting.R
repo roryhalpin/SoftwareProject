3b97dc3769ce046423cb1bce80cc14b292458ad8
Rory Halpin <roryhalpin@users.noreply.github.com>
2019-12-16 21:22:53 +0000

    Update locationTesting.R
    Aattempting to parse location data to mapper

--- KDD/Data/locationTesting.R
+++ KDD/Data/locationTesting.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(dplyr)
 library(Amelia)
 library(sf)
+library(stringr)
 
 print(getwd())
 
@@ -15,3 +16,14 @@ print(loc)
 
 missmap(loc, main = "Missing values in junctions.kml")
 
+loc_sample <- loc[sample(1:nrow(loc), 50,
+                          replace=FALSE),]
+
+print(loc_sample)
+
+df = subset(loc_sample, select = -c(Name,Description) )
+
+print(df)
+
+
+dataframe %>% extract(x, c("Latitude", "Longitude"), "\\(([^,]+), ([^)]+)\\)")
