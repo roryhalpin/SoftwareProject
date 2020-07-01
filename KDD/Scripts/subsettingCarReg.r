06e62535269f2a24d3a2e4a4ae7089f2e322f9a5
Rory Halpin <roryhalpin@users.noreply.github.com>
2020-07-01 14:41:26 +0100

    updated code and processed data

--- KDD/Scripts/subsettingCarReg.r
+++ KDD/Scripts/subsettingCarReg.r
@@ -3,15 +3,39 @@ library(tidyverse)
 setwd("C:/Users/sex-i/OneDrive/Documents/GitHub/Project/KDD/Data")
 read.csv("RawCarReg.csv") -> rawCarReg
 
+# Rename corrupted LOCATION column
 rawCarReg <- rawCarReg %>% 
   rename(
     LOCATION = ï..LOCATION
   )
 
+# Display 
 rawCarReg
 
-IRLCarRegA <- subset(rawCarReg, LOCATION =='IRL' & FREQUENCY =='A', select = c(LOCATION, FREQUENCY, TIME, Value))
-IRLCarRegM <- subset(rawCarReg, LOCATION =='IRL' & FREQUENCY =='M', select = c(LOCATION, FREQUENCY, TIME, Value))
+# Drop columns we're not going to use both by name
+rawCarReg <- select(rawCarReg, -c(SUBJECT,INDICATOR,MEASURE, Flag.Codes))
 
-IRLCarRegA
+# Creating dataframe with just monthly frequency data
+rawMonthlyData <- subset(rawCarReg, FREQUENCY == 'M') 
+
+# Creating several Monthly and anual dataframes for different countries for initial testing
+IRLCarRegM <- subset(rawCarReg, LOCATION =='IRL' & FREQUENCY =='M', select = c(LOCATION, FREQUENCY, TIME, Value))
+CANCarRegM <- subset(rawCarReg, LOCATION =='CAN' & FREQUENCY =='M', select = c(LOCATION, FREQUENCY, TIME, Value))
+
+# Display out new dataframes
 IRLCarRegM
+
+# Split the rawcar dataframe by each individual locaiton
+# split(rawCarReg, rawCarReg$LOCATION)
+rawMonthlyData
+rawMonthlyData <- spread(rawMonthlyData, key = "LOCATION" ,value = "Value")
+rawMonthlyData
+
+# Making subset of rawmonthlydata with several countries that have NA values
+rawMonthlyRemoveNAS <- subset(rawMonthlyData, select=c(FREQUENCY, TIME, BEL, CHE, DEU, DNK, FIN, ITA))
+rawMonthlyRemoveNAS <- na.omit(rawMonthlyRemoveNAS)
+
+# Write dataframes to a csv files
+write.csv(IRLCarRegM, "C:/Users/sex-i/OneDrive/Documents/GitHub/Project/KDD/Data/IRLCarRegM.csv", row.names = TRUE)
+write.csv(CANCarRegM, "C:/Users/sex-i/OneDrive/Documents/GitHub/Project/KDD/Data/CANCarRegM.csv", row.names = TRUE)
+write.csv(rawMonthlyData, "C:/Users/sex-i/OneDrive/Documents/GitHub/Project/KDD/Data/GlobalCarRegM.csv", row.names = TRUE)
